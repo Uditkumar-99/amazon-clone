3d9a85f007ae9a350c46030db9126096e340185a
Uditkumar-99 <mouff3663@gmail.com>
2026-03-08 21:22:27 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# amazon.clone
+A small test to make the copy of Amazon. com home page clone using html and css 
+link to watch the website - 
+https://sathtikbose.github.io/amazon.clone/
---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("pd1").addEventListener("click", function() {
+    window.location.href = "pd1.html"; 
+});
+document.getElementById("pd2").addEventListener("click", function() {
+    window.location.href = "pd2.html"; 
+});
+document.getElementById("pd3").addEventListener("click", function() {
+    window.location.href = "pd3.html"; 
+});
+document.getElementById("pd4").addEventListener("click", function() {
+    window.location.href = "pd4.html"; 
+});
+document.getElementById("pd5").addEventListener("click", function() {
+    window.location.href = "pd5.html"; 
+});
+document.getElementById("pd6").addEventListener("click", function() {
+    window.location.href = "pd6.html"; 
+});
+document.getElementById("pd7").addEventListener("click", function() {
+    window.location.href = "pd7.html"; 
+});
+document.getElementById("pd8").addEventListener("click", function() {
+    window.location.href = "pd8.html"; 
+});
